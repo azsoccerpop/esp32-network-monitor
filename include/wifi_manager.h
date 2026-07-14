39ea3656677db09d273b51434ed165fce02dc7b8
Stephen J Anderson <83408574+azsoccerpop@users.noreply.github.com>
2026-07-14 06:13:15 -0700

    Update wifi_manager.h

--- include/wifi_manager.h
+++ include/wifi_manager.h
@@ -1,1 +1,16 @@
-// Added isPortalActive() method
+#pragma once
+#include <Arduino.h>
+
+class WifiManager {
+public:
+  static void begin();
+  static void loop();
+  static bool isConnected();
+  static String getLocalIP();
+  static void startConfigPortal();
+  static bool isPortalActive();   // NEW: for DisplayManager
+private:
+  static void loadWifiSettings();
+  static void saveWifiSettings(const String& ssid, const String& password);
+  static bool tryConnect();
+};
